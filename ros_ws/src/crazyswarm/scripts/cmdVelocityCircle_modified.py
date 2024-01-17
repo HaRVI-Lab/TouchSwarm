66e67e2869856ee25346ed0e659ac4e1dd9881c6
chen716 <chen716@usc.edu>
2024-01-17 13:10:28 -0800

    update logging

--- ros_ws/src/crazyswarm/scripts/cmdVelocityCircle_modified.py
+++ ros_ws/src/crazyswarm/scripts/cmdVelocityCircle_modified.py
@@ -5,43 +5,44 @@ import numpy as np
 from pycrazyswarm import *
 import logging
 import time
+#!/usr/bin/env python
+import rospy
+from crazyswarm.msg import GenericLogData
+import datetime
+import os
+
+# Function to handle incoming log data
+def log_callback(data):
+    # Open the file in append mode
+    with open(file_path, 'a') as file:
+        # Write data to file; you might need to adjust this depending on the actual structure of GenericLogData
+        file.write(str(data) + '\n')
+
+# Initialize the ROS node
+#rospy.init_node('log_listener')
+
+
 
 # Initialize logging to a file
-logging.basicConfig(filename='/home/harvilab/logs/SidewayTest/crazyflie_log.txt', level=logging.INFO,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
 
-Z = 0.80
+Z = 0.750
 sleepRate = 30
 
-# Callback function for logging state estimate and control target
-def log_callback(timestamp, data, logconf):
-    # Log the data from the subscribed log variables
-    print("Data: Timestamp: {timestamp}, Data: {data}")
 
 # Main control function
 def goCircle(timeHelper, cf, totalTime, radius, kPosition):
     # Log configuration for the Crazyflie
-    # logconf = LogConfig(name='Position', period_in_ms=100)
-    # logconf.add_variable('stateEstimate.x', 'float')
-    # logconf.add_variable('ctrltarget.x', 'float')
 
-    # # Start logging
-    # cf.log.add_config(logconf)
-    # logconf.data_received_cb.add_callback(log_callback)
-    # logconf.start()
-    desiredPos = np.array([0, 0, Z])
     startTime = timeHelper.time()
     while timeHelper.time() - startTime < totalTime:
         # Get the current time and position
         current_time = timeHelper.time() - startTime
         current_position = cf.position()
         
-        # Log the current position from cf.position()
-        # logging.info(f"Current Position: {current_position}")
-
+        
         # Compute the desired position
-        
-        
+        desiredPos = np.array([0, 0, Z])
+
         # Compute the error vector
         error = desiredPos - current_position
         distance = np.linalg.norm(error)
@@ -50,8 +51,8 @@ def goCircle(timeHelper, cf, totalTime, radius, kPosition):
         if distance > 0.2:
             kPosition = 0  # Stop motion if outside 20 cm radius
         else:
-            kPosition = 2.5  # Allow motion within 20 cm radius
-        print("POS:" + str(cf.position()[0])+","+str(cf.position()[1])+","+str(cf.position()[2])+","+str(kPosition))
+            kPosition = 1  # Allow motion within 20 cm radius
+
         # Command the drone with velocity control
         cf.cmdVelocityWorld(np.array([0, 0, 0]) + kPosition * error, yawRate=0)
 
@@ -59,15 +60,24 @@ def goCircle(timeHelper, cf, totalTime, radius, kPosition):
         timeHelper.sleepForRate(sleepRate)
 
     # Stop the log configuration
-    #logconf.stop()
+   
 
 if __name__ == "__main__":
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
+    # Get the current time for the file name
+    now = datetime.datetime.now()
+    file_name = now.strftime("%Y_%m_%d-%H_%M_%S_state.log")
+    file_path = os.path.join("/home/harvilab/logs/cmdVel", file_name)  # Set the directory path where you want to save the file
 
+    # Subscribe to the /cf1/log1 topic
+    rospy.Subscriber("/cf1/log1", GenericLogData, log_callback)
+
+    # Spin to keep the script for exiting
+    #rospy.spin()
     allcfs.takeoff(targetHeight=Z, duration=1.0+Z)
     timeHelper.sleep(2 + Z)
-    goCircle(timeHelper, allcfs.crazyflies[0], totalTime=30, radius=1, kPosition=1)
+    goCircle(timeHelper, allcfs.crazyflies[0], totalTime=40, radius=1, kPosition=1)
     allcfs.land(targetHeight=0.02, duration=1.0+Z)
     timeHelper.sleep(1.0+Z)
--- ros_ws/src/crazyswarm/scripts/hello_world.py
+++ ros_ws/src/crazyswarm/scripts/hello_world.py
@@ -1,19 +1,81 @@
 """Takeoff-hover-land for one CF. Useful to validate hardware config."""
 
 from pycrazyswarm import Crazyswarm
-
+import numpy as np
 
 TAKEOFF_DURATION = 2.5
 HOVER_DURATION = 5.0
+totalTime = 25.0
+import rospy
+from crazyswarm.msg import GenericLogData
+import datetime
+import os
+z = 0.75
+radius = 0.15
+now = datetime.datetime.now()
+    
+goal = np.array([0,0,z])
+statefile_name = now.strftime("%Y_%m_%d-%H_%M_%S_state.log")
+posfile_name = now.strftime("%Y_%m_%d-%H_%M_%S_pos.log")
+file_path = os.path.join("/home/harvilab/logs/cmdVel", statefile_name)  # Set the directory path where you want to save the file
+file_path2 = os.path.join("/home/harvilab/logs/cmdVel", posfile_name)  # Set the directory path where you want to save the file
 
+# Function to handle incoming log data
+def log_callback(data):
+    # Open the file in append mode
+    with open(file_path, 'a') as file:
+        # Write data to file; you might need to adjust this depending on the actual structure of GenericLogData
+        file.write("***************"+str(data) +'\n')
+        #print(str(data) +'\n')
+    
 
 def main():
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     cf = swarm.allcfs.crazyflies[0]
+    
+    # Subscribe to the /cf1/log1 topic
+    rospy.Subscriber("/cf1/log1", GenericLogData, log_callback)
 
-    cf.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
-    timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
+    
+    cf.takeoff(targetHeight=z, duration=TAKEOFF_DURATION)
+    timeHelper.sleep(TAKEOFF_DURATION)
+    startTime = timeHelper.time()
+    print("starting")
+    with open(file_path2, 'a') as file2:
+    # Write data to file; you might need to adjust this depending on the actual structure of GenericLogData
+        
+        while timeHelper.time() - startTime < totalTime:
+            desiredPos = np.array([0, 0, z])
+            error = desiredPos - cf.position()
+            distance = np.linalg.norm(error)
+            goal = np.array([0,0,z])
+            
+            file2.write(str(timeHelper.time())+","+str(cf.position()[0])+ ","+str(cf.position()[1])+","+ str(cf.position()[2])+","+ str(distance)+'\n')
+            print(str(cf.position()[0])+ ","+str(cf.position()[1])+","+ str(cf.position()[2])+","+ str(distance)+' ****************\n')
+            # Apply haptic feedback within 20 cm of the center
+            # if distance < radius:
+                
+            #     # print(str(goal[0])+ ","+str(goal[1])+","+ str(goal[2])+str(distance)+'\n')
+            #     goal = np.array([0,0,z]) # Stop motion if outside 20 cm radius
+            # else:
+            #     #goal = np.array([-1,-1,z])
+            #     goal = cf.position() # Sow motion 
+            #print("***********************"+str(goal[0])+ ","+str(goal[1])+","+ str(goal[2])+str(distance)+'\n')
+
+            #     #print("in staing other pos")
+            #     goal[2] = z
+
+            # file.write(str(goal[0])+ ","+str(goal[1])+","+ str(goal[2])+'\n')
+            
+            #cf.cmdPosition(goal, yaw =  0)
+
+
+            timeHelper.sleepForRate(80)
+
+
+
+
     cf.land(targetHeight=0.04, duration=2.5)
     timeHelper.sleep(TAKEOFF_DURATION)
 
